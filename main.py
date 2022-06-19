21f436344a7b6fe533153f68450f1545baf73343
Tiago Gaspar <tiagongaspar@gmail.com>
2022-06-19 22:08:51 +0100

    Cleaned formating using flake8 and black

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -7,16 +6,21 @@ from datetime import datetime
 from utils.url_inspector import validate_url
 from utils.archive import get_url_archive
 from starlette.responses import RedirectResponse
-# secrets file: 
+
+# secrets file:
 import config
 
 
-app=FastAPI(title="SaveMyPage API", description="An API so save and archive pages for later access")
+app = FastAPI(
+    title="SaveMyPage API",
+    description="An API so save and archive pages for later access",
+)
 templates = Jinja2Templates(directory="templates")
-deta=Deta(config.deta_private_key)
+deta = Deta(config.deta_private_key)
 
-#creates saved_urls database
-db=deta.Base('saved_urls')
+# creates saved_urls database
+db = deta.Base("saved_urls")
+
 
 # call and insert archive
 def _archive(url):
@@ -25,70 +29,75 @@ def _archive(url):
         url_records = db.fetch({"url": url})
         keys = [item["key"] for item in url_records.items]
         for key in keys:
-            db.update( {"archive_url": archive_url} , key)
-    except:
-        print("Unable to generate archive URL for " + url)
+            db.update({"archive_url": archive_url}, key)
+    except Exception as ex:
+        print(ex)
+
 
 # Adds a url to lisk of saved urls
-@app.post('/save/{title}/{url:path}')
-async def add_url(title:str, url:str):
-    #validate url
-    if validate_url(url) == False:
+@app.post("/save/{title}/{url:path}")
+async def add_url(title: str, url: str):
+    # validate url
+    if validate_url(url) is False:
         return {"message": "url {url} not in format http(s)://website.com"}
     # insert to the database
-    db.insert({"title":title, "url":url, "timestamp":datetime.now().timestamp()})
+    db.insert({"title": title, "url": url,
+               "timestamp": datetime.now().timestamp()})
     # not working due to timeout
-    #_archive(url)
+    # _archive(url)
     # inserts a note with key as id
     print(f"Url inserted: {title} - {url}")
-    return {"message":"url successfully added"}
+    return {"message": "url successfully added"}
 
 
 # delete route takes the url id to delete the record from db
-@app.delete('/delete/{id}')
-async def delete_url(id:str):
+@app.delete("/delete/{id}")
+async def delete_url(id: str):
     db.delete(id)
-    return {"message":f"url with id {id} deleted"}
+    return {"message": f"url with id {id} deleted"}
 
 
 # get all records in db
-@app.get('/getall')
+@app.get("/getall")
 async def get_all_url():
     # fetches all records from db
-    all_urls=db.fetch()
+    all_urls = db.fetch()
     return all_urls.items
 
 
 # get all records in db
-@app.get('/', response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse)
 async def get_all_url2(request: Request):
     # fetches all records from db
-    all_urls=db.fetch()
-    return templates.TemplateResponse("index.html", {"request": request, "items": all_urls.items})
+    all_urls = db.fetch()
+    return templates.TemplateResponse(
+        "index.html", {"request": request, "items": all_urls.items}
+    )
 
 
-@app.get('/deleteUI/{key}', response_class=HTMLResponse)
-async def deleteUI(request: Request, key:str):
+@app.get("/deleteUI/{key}", response_class=HTMLResponse)
+async def deleteUI(request: Request, key: str):
     # fetches all records from db
     db.delete(key)
-    return RedirectResponse('/')
+    return RedirectResponse("/")
 
-@app.get('/saveUI', response_class=HTMLResponse)
-async def saveUI(request: Request, title:str, url:str):
-    if validate_url(url) == False:
+
+@app.get("/saveUI", response_class=HTMLResponse)
+async def saveUI(request: Request, title: str, url: str):
+    if validate_url(url) is False:
         return {"message": "url {url} not in format http(s)://website.com"}
     # insert to the database
-    db.insert({"title":title, "url":url, "timestamp":datetime.now().timestamp()})
+    db.insert({"title": title, "url": url,
+               "timestamp": datetime.now().timestamp()})
     # not working due to timeout
-    #_archive(url)
-    return RedirectResponse('/')
+    # _archive(url)
+    return RedirectResponse("/")
+
 
 # replace with key
-@app.get('/archiveUI/{url:path}', response_class=HTMLResponse) 
-async def archiveUI(request: Request, url:str):
+@app.get("/archiveUI/{url:path}", response_class=HTMLResponse)
+async def archiveUI(request: Request, url: str):
     # insert to the database
     # not working due to timeout
     _archive(url)
-    return RedirectResponse('/')
-
-
+    return RedirectResponse("/")
